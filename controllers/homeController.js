bb71d750b4b16061e9c371d8d2b9f1c437de5b81
Gimena Sánchez <gmnsanchezunicah@gmail.com>
2019-10-25 00:40:34 -0600

    Reparación de la redirección del título de la página, como que cargue el nombre de el usuario logueado en la pagina que se requiere.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,6 +4,9 @@ const Cuenta = mongoose.model("Cuenta");
 exports.mostrarbalance = async (req, res, next) => {
   // Obtener todos los documentos de las cuentas
   const cuentas = await Cuenta.find();
+
+  const cuentita = await Cuenta.find({ autor: req.user._id });
+
   console.log(req.user._id);  //obtenemos el usuario que esta logeado esta es la variabla que lo almacena 
   const cuentastipo = await Cuenta.aggregate([
     {
@@ -51,7 +54,8 @@ exports.mostrarbalance = async (req, res, next) => {
     descripcion:"Le ayudamos a gestionar su economía personal compruebe aquí el estado de sus cuentas, empiece a hacer el presupuesto y organice sus deudas.",
     crearCuenta: "Conozca los pasos a seguir para evitar sorpresas al invertir sus ahorros: defina su perfil y vigile sus inversiones.",
     inicioSesion: "!Accede ya! y continua manejando de manera eficiente y eficaz tus finanzas",
-   
+    nombre: req.user.nombre,
+    cuentita,
     cuentas,
     balance,
     ingreso,
